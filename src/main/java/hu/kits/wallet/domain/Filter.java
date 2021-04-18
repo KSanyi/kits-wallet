29f5bd6e8aec5ec06f20de0b01f3acbaccb8f7cd
Kocso Sandor Gabor <kocso.sandor.gabor@gmail.com>
2021-04-18 17:40:39 +0200

    use records

--- src/main/java/hu/kits/wallet/domain/Filter.java
+++ src/main/java/hu/kits/wallet/domain/Filter.java
@@ -1,21 +1,6 @@
 package hu.kits.wallet.domain;
 
-public class Filter {
+public record Filter(String filterString, DateInterval dateInterval) {
 
-    private final String filterString;
-    private final DateInterval dateInterval;
-    
-    public Filter(String filterString, DateInterval dateInterval) {
-        this.filterString = filterString;
-        this.dateInterval = dateInterval;
-    }
-
-    public String filterString() {
-        return filterString;
-    }
-    
-    public DateInterval dateInterval() {
-        return dateInterval;
-    }
     
 }
