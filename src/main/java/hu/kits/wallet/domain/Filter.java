445060a307ecd60943732211a9b0a944e015815e
Kocso Sandor Gabor <kocso.sandor.gabor@gmail.com>
2021-04-18 17:46:59 +0200

    do not use records

--- src/main/java/hu/kits/wallet/domain/Filter.java
+++ src/main/java/hu/kits/wallet/domain/Filter.java
@@ -1,6 +1,21 @@
 package hu.kits.wallet.domain;
 
-public record Filter(String filterString, DateInterval dateInterval) {
+public class Filter {
 
+    private final String filterString;
+    private final DateInterval dateInterval;
+    
+    public Filter(String filterString, DateInterval dateInterval) {
+        this.filterString = filterString;
+        this.dateInterval = dateInterval;
+    }
+
+    public String filterString() {
+        return filterString;
+    }
+    
+    public DateInterval dateInterval() {
+        return dateInterval;
+    }
     
 }
