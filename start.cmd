8ffd26b730d4decd381a49127d3ed93426675c21
Sandor Gabor Kocso <kocso.sandor.gabor@gmail.com>
2022-01-04 14:34:55 +0100

    version upgrades

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
 SET CLEARDB_DATABASE_URL=mysql://b7574304cef6f2:4d1ff9f8@eu-cdbr-west-02.cleardb.net/heroku_0e8a3eb7b59ea61?reconnect=true
 SET PORT=5555
-java --enable-preview -jar target/app.jar
+java -jar target/app.jar
